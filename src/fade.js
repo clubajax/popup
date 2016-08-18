be511fec254154d1741fbaebd5eab02b5607f6ae
Mike Wilcox <mwilcox@paycor.com>
2016-08-18 16:57:48 -0500

    Updates for fx

--- src/fade.js
+++ src/fade.js
@@ -6,20 +6,24 @@
         type: 'animate',
         name: 'fade',
         show: function (options, popNode, input, callback) {
-            fx.opacity(popNode, {
-                startOpacity: 0,
-                opacity: 1,
+            fx.style(popNode, {
+                opacity:{
+                    beg: 0,
+                    end: 1
+                },
                 speed: options.speed,
                 callback: callback
-            })
+            });
         },
         hide: function (options, popNode, input, callback) {
-            fx.opacity(popNode, {
-                startOpacity: 1,
-                opacity: 0,
+            fx.style(popNode, {
+                opacity:{
+                    beg: 1,
+                    end: 0
+                },
                 speed: options.speed,
                 callback: callback
-            })
+            });
         }
     });
 
--- src/wipe.js
+++ src/wipe.js
@@ -3,22 +3,25 @@
     popup.addPlugin({
         type: 'animate',
         name: 'wipe',
-        show: function (options, popup, input, callback) {
-            var node = options.aniNode || popup;
-            fx.height(node, {
-                startHeight: 0,
-                //height: 'auto',
-                height: dom.style(node, 'height') || 'auto',
+        show: function (options, popNode, input, callback) {
+            fx.style(options.aniNode || popNode, {
+                height:{
+                    beg: 0,
+                    end: 'auto'
+                },
                 speed: options.speed,
                 callback: callback
-            })
+            });
         },
-        hide: function (options, popup, input, callback) {
-            fx.height(options.aniNode || popup, {
-                height: 0,
+        hide: function (options, popNode, input, callback) {
+            fx.style(options.aniNode || popNode, {
+                height:{
+                    beg: 'auto',
+                    end: 0
+                },
                 speed: options.speed,
                 callback: callback
-            })
+            });
         }
     });
 
